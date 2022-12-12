a3d61dc0f101e0428ce435fd5ee7155a75b23c23
Lukáš <lukasjanouch8@gmail.com>
2022-12-12 12:06:20 +0100

    feat: added button to add image input in new-album.html

--- src/main/resources/static/js/add-image-input.js
+++ src/main/resources/static/js/add-image-input.js
@@ -1,11 +1,8 @@
 function addInputLine() {
-    var label = document.createElement("label");
-    var input = document.createElement("input");                 // Create an <input> node
+    const input = document.createElement("input");// Create an <input> node
 
-    label.setAttribute("id", "add-image-label");
     input.setAttribute("type", "file");
     input.setAttribute("th:field", "*{image}");
 
-    document.getElementById("add-album-div").appendChild(label);// Append it to the parent
-    document.getElementById("add-image-label").appendChild(input);
+    document.getElementById("add-album-div").appendChild(input);// Append it to the parent
 }
